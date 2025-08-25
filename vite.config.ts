e691b3c64ae0d85c5dba38dd15e21e049f67e377
Love4e <uzunov.angel@abv.bg>
2025-08-25 20:26:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// нужно за GitHub Pages (deploy в /lovelink-mvp/)
 export default defineConfig({
   plugins: [react()],
-  base: '/lovelink-mvp/',
+  base: '/lovelink-mvp/', // ← името на репото (ако е друго, смени)
 })
