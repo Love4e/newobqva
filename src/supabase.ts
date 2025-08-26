c7352f30755c10dfa375ae15374c9d7fe18566eb
Love4e <uzunov.angel@abv.bg>
2025-08-26 21:24:09 +0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+// src/supabase.ts
+import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_ANON_KEY!,
-  {
-    auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true },
-    realtime: { params: { eventsPerSecond: 10 } },
-  }
+  'https://gazaegcwedqiyaefkgsr.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhemFlZ2N3ZWRxaXlhZWZrZ3NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNDU3NzYsImV4cCI6MjA3MTcyMTc3Nn0.DCz7PhdKzQiOGgQwjgZ3JdOS4LfB-Bmb32VatfRsHB8',
+  { auth: { persistSession: true, autoRefreshToken: true } }
 );
