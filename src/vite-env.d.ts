12081d31b164251e8c77d098b244dde8c7fc2d56
Love4e <uzunov.angel@abv.bg>
2025-08-27 21:37:02 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string
-  readonly VITE_SUPABASE_ANON_KEY: string
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.webp';
+declare module '*.gif';
+declare module '*.css';
