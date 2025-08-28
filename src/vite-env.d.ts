b0f321de93e218f5a1c96255636cb959bc6d42b6
Love4e <uzunov.angel@abv.bg>
2025-08-28 18:58:39 +0300

    Delete src directory

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.svg';
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.webp';
-declare module '*.gif';
-declare module '*.css';
