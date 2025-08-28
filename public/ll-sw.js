3bed36d7bd55e40d07933be58822412b9ecc9347
Love4e <uzunov.angel@abv.bg>
2025-08-28 18:58:31 +0300

    Delete public directory

--- public/ll-sw.js
+++
@@ -1,5 +0,0 @@
-// много лек service worker – без кеширане, само за регистрация
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
